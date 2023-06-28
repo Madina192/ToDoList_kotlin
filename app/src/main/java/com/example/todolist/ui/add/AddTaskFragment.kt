7a8858804c5ea054b432e90b25b35c13af0da956
Madina <madinaakpaeva000@gmail.com>
2023-06-28 15:17:13 +0600

    added handleClick fun

--- app/src/main/java/com/example/todolist/ui/add/AddTaskFragment.kt
+++ app/src/main/java/com/example/todolist/ui/add/AddTaskFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.todolist.databinding.FragmentAddBinding
+import com.example.todolist.model.Task
 
 class AddTaskFragment : Fragment() {
 
@@ -19,6 +21,18 @@ class AddTaskFragment : Fragment() {
     ): View {
         _binding = FragmentAddBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        handleClick()
+    }
+
+    private fun handleClick(){
+        binding.btnSaveTask.setOnClickListener{
+            findNavController().navigateUp()
+        }
     }
 
     override fun onDestroyView() {
